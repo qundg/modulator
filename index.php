90a495a45c8ed0f427b6f362553da6bb1ab41a51
Dennis Hingst <post@dennis-hingst.de>
2015-02-26 13:33:14 +0100

    Verschachtelte Ifs und Variablen ermöglicht

--- index.php
+++ index.php
@@ -16,23 +16,25 @@ if(!defined('ABSPATH')) die();
  * Module im Unterverzeichnis modules des aktuellen Themes automatisch einbinden
  */
 $modules_path = get_template_directory() . '/modules';
-$module_dirs = scandir($modules_path);
-foreach ($module_dirs as $module_dir) {
-    $module_path = $modules_path . '/' . $module_dir;
+if (is_dir($modules_path)) {
+    $module_dirs = scandir($modules_path);
+    foreach ($module_dirs as $module_dir) {
+        $module_path = $modules_path . '/' . $module_dir;
 
-    // Verzeichnisse mit . im Namen ignorieren
-    if (strpos($module_dir, '.') !== false OR !is_dir($module_path)) {
-        continue;
-    }
+        // Verzeichnisse mit . im Namen ignorieren
+        if (strpos($module_dir, '.') !== false OR !is_dir($module_path)) {
+            continue;
+        }
 
-    // composer.php soll Klasse für den Visual Composer enthalten
-    if (file_exists($module_path . '/composer.php')) {
-        require_once($module_path . '/composer.php');
-    }
+        // composer.php soll Klasse für den Visual Composer enthalten
+        if (file_exists($module_path . '/composer.php')) {
+            require_once($module_path . '/composer.php');
+        }
 
-    // generic.php sollte eine Funktion für eine generische Darstellung des Elements enthalten
-    if (file_exists($module_path . '/generic.php')) {
-        require_once($module_path . '/generic.php');
+        // generic.php sollte eine Funktion für eine generische Darstellung des Elements enthalten
+        if (file_exists($module_path . '/generic.php')) {
+            require_once($module_path . '/generic.php');
+        }
     }
 }
 
@@ -49,12 +51,13 @@ class Modulator {
 
 
     /**
-     * Neues Modul erzeugen
+     * Modul erzeugen
      *
-     * @param $name string Verzeichnisname des Moduls (in ../modules)
+     * @param string $name Verzeichnisname des Moduls
+     * @throws Exception Wenn kein Modul mit diesem Namen existiert
      */
-    function __construct ($name) {
-        // einmal Basispfad und -URL für alle Module definieren
+    function __construct($name) {
+        // einmalig Basispfad und -URL für alle Module definieren
         if (empty(self::$base_path) OR empty(self::$base_url)) {
             self::$base_path = get_template_directory() . '/modules';
             self::$base_url = get_template_directory_uri() . '/modules';
@@ -68,7 +71,8 @@ class Modulator {
         $this->module_path = self::$base_path . '/' . $name;
         $this->module_url = self::$base_url . '/' . $name;
 
-        if (!is_dir($this->module_path) OR !file_exists($this->module_path) . '/index.html') {
+        // Fehler, wenn kein Verzeichnis mit dem Namen existiert oder keine index.html enthalten ist
+        if (!is_dir($this->module_path) OR !file_exists($this->module_path . '/index.html')) {
             throw New Exception(sprintf('Kein Modul mit dem Namen %s gefunden.', $name));
         }
 
@@ -86,11 +90,11 @@ class Modulator {
      *
      * Variablen werden in der index.html des Moduls im Format %%name%% angelegt.
      *
-     * @param $name string Name der Variable
-     * @param $value string Wert der Variable
+     * @param string $name Name der Variable
+     * @param string $value Wert der Variable
      */
-    public function set_var ($name, $value) {
-        $this->vars[$name] = strval($value);
+    public function set_var($name, $value) {
+        $this->vars[$name] = $value;
     }
 
 
@@ -104,25 +108,63 @@ class Modulator {
      *   ...
      * %%/loop:NAME%%
      *
-     * @param $loop_name string Name des Loops
-     * @param $new_item array Werte des Elements, z.B. array('titel' => 'beispiel', 'content' => 'blah blah')
+     * @param string $loop_name Name des Loops
+     * @param array $new_item Werte des Elements, z.B. array('titel' => 'beispiel', 'content' => 'blah blah')
      */
-    public function add_loop_item ($loop_name, $new_item) {
+    public function add_loop_item($loop_name, $new_item) {
         // Loop anlegen, falls dies das erste Elemente für den Loop ist
         if (!isset($this->loops[$loop_name])) {
             $this->loops[$loop_name] = array();
         }
 
-        // alle Schlüssel als LOOPNAME.VARNAME formatieren
-        $formatted_item = array();
-        foreach ($new_item as $key => $value) { // alle Werte des Items durchlaufen, jeweils z.B. 'name' => 'Beispiel GmbH'
-            $formatted_item[$loop_name . '.' . $key] = $value; // z.B. 'kunden.name' => 'Beispiel GmbH'
-        }
-        $this->loops[$loop_name][] = $formatted_item;
+        $this->loops[$loop_name][] = $new_item;
     }
 
 
-    private function parse_loops ($template) {
+    /**
+     * Ifs in einem Template-String verarbeiten
+     *
+     * @param string $template HTML des Templates
+     * @param array $vars Variablen und ihre Werte zum Prüfen der Ifs (je 'varname' => 'Wert')
+     * @param string $prefix Präfix für Ifs, z.B. wäre bei %%if:loopname.varname%% der Präfix 'loopname'
+     * @return string Template-String mit eingesetzten Ifs
+     */
+    private function parse_ifs($template, $vars, $prefix = '') {
+        if ($prefix) {
+            $regex = '/%%if:' . $prefix . '\.(.*)%%(.*)%%\/if:' . $prefix . '\.\1%%/misU'; // %%if:PREFIX.match1%% match2 %%/if:PREFIX.match1%%
+        } else {
+            $regex = '/%%if:([^.]*)%%(.*)%%\/if:\1%%/misU'; // %%if:match1%% match2 %%/if:match1%% - match1 ohne .
+        }
+
+        $matches = array();
+        preg_match_all($regex, $template, $matches, PREG_SET_ORDER);
+
+        foreach ($matches as $match) {
+            $replace = $match[0]; // kompletter Match inkl %%if:NAME%% und %%/if:NAME%%
+            $condition = $match[1];
+            $content = $match[2];
+
+            // prüfen, ob eine Variable zu dieser Condition vorliegt und ob sie == true ist
+            // nicht ===, damit auch Strings als Condition genutzt werden können
+            if (isset($vars[$condition]) AND $vars[$condition] == true) {
+                $template = str_replace($replace, $content, $template);
+            } else { // kompletten if-Block entfernen, wenn weder Condition noch Variable mit dem Namen vorliegen
+                $template = str_replace($replace, '', $template);
+            }
+        }
+
+        return $template;
+    }
+
+
+    /**
+     * Loops in einem Template-String verarbeiten
+     *
+     * @param string $template HTML des Templates
+     * @param array $loops Zu verarbeitende Loops (je 'loopname' => array('varname' => 'Wert'))
+     * @return string Template-String mit verarbeiteten Loops
+     */
+    private function parse_loops($template, $loops) {
         $matches = array();
         $regex = '/%%loop:(.*)%%(.*)%%\/loop:\1%%/misU'; // %%loop:match1%% match2 %%/loop:match1%%
         preg_match_all($regex, $template, $matches, PREG_SET_ORDER);
@@ -132,10 +174,12 @@ class Modulator {
             $loop = $match[1];
             $content = $match[2];
 
-            if (isset($this->loops[$loop])) {
+            if (isset($loops[$loop])) {
                 $loop_content = '';
-                foreach ($this->loops[$loop] as $loop_vars) {
-                    $loop_content .= $this->parse_ifs($content, $loop_vars);
+                foreach ($loops[$loop] as $loop_vars) {
+                    $content = $this->parse_ifs($content, $loop_vars, $loop);
+                    $content = $this->insert_vars($content, $loop_vars, $loop);
+                    $loop_content .= $content;
                 }
                 $template = str_replace($replace, $loop_content, $template);
             } else {
@@ -147,32 +191,22 @@ class Modulator {
     }
 
 
-    private function parse_ifs ($template) {
-        $matches = array();
-        $regex = '/%%if:(.*)%%(.*)%%\/if:\1%%/misU'; // %%if:match1%% match2 %%/if:match1%%
-        preg_match_all($regex, $template, $matches, PREG_SET_ORDER);
-
-        foreach ($matches as $match) {
-            $replace = $match[0]; // kompletter Match inkl %%if:NAME%% und %%/if:NAME%%
-            $condition = $match[1];
-            $content = $match[2];
-
-            // prüfen, ob eine Variable zu dieser Condition vorliegt und ob sie == true ist
-            // nicht ===, damit auch Strings als Condition genutzt werden können
-            if (isset($this->vars[$condition]) AND $this->vars[$condition] == true) {
-                $template = str_replace($replace, $content, $template);
-            } else { // kompletten if-Block entfernen, wenn weder Condition noch Variable mit dem Namen vorliegen
-                $template = str_replace($replace, '', $template);
-            }
+    /**
+     * Variablen in einen Template-String einsetzen
+     *
+     * @param string $template HTML des Templates
+     * @param array $vars Einzusetzende Variablen (je 'varname' => 'Wert')
+     * @param string $prefix Präfix für Variablen, z.B. wäre bei %%loopname.varname%% der Präfix 'loopname'
+     * @return string Template-String mit eingesetzten Variablen
+     */
+    private function insert_vars($template, $vars, $prefix = '') {
+        if ($prefix) {
+            $regex = '/%%' . $prefix . '\.(.*)%%/misU'; // %%PREFIX.match%%
+        } else {
+            $regex = '/%%([^.]*)%%/misU'; // %%match%% ohne .
         }
 
-        return $template;
-    }
-
-
-    private function insert_vars ($template, $vars) {
         $matches = array();
-        $regex = '/%%(.*)%%/misU'; // %%match%%
         preg_match_all($regex, $template, $matches, PREG_SET_ORDER);
 
         foreach ($matches as $match) {
@@ -193,13 +227,13 @@ class Modulator {
     /**
      * Ifs, Loops und Variablen im HTML ersetzen und Template ausgeben
      */
-    public function output () {
+    public function output() {
         // Template-HTML holen
         $template = file_get_contents($this->module_path . '/index.html');
 
-        $template = $this->parse_loops($template);
+        $template = $this->parse_ifs($template, $this->vars);
+        $template = $this->parse_loops($template, $this->loops);
         $template = $this->insert_vars($template, $this->vars);
-        $template = $this->parse_ifs($template);
 
         echo $template;
     }
